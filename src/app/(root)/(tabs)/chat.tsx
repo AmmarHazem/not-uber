6e8141df809ff5438f72e71d1d7bfafb3109ee16
Ammar <ammar@ovasave.com>
2025-05-19 21:10:53 +0400

    setting up main tabs

---
+++ src/app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Text, View } from "react-native";
+
+const Chat: FC = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Chat</Text>
+    </View>
+  );
+};
+
+export default Chat;
---
+++ src/app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Text, View } from "react-native";
+
+const Home: FC = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
