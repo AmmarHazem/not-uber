107341181a9975ea8a9f315dbb588b3becf6f1bb
Ammar <ammar@ovasave.com>
2025-05-22 22:10:21 +0400

    building welcome screen

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,11 +1,26 @@
+import PrimaryButton from "@/src/components/PrimaryButton";
 import { FC } from "react";
-import { Text, View } from "react-native";
+import { Image, Pressable, SafeAreaView, Text, View } from "react-native";
 
 const Welcome: FC = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Welcome</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white flex-col justify-between">
+      <View className="pt-10 flex-row justify-end px-8 items-center">
+        <Pressable hitSlop={16}>
+          <Text className="font-semibold">Skip</Text>
+        </Pressable>
+      </View>
+      <View className="flex-1">
+        <Image source={require("@/assets/images/onboarding1.png")} className="w-full h-[400px]" resizeMode="contain" />
+        <Text className="text-4xl px-4 font-bold text-center mb-4">The best car in your hand with Ryde</Text>
+        <Text className="text-center px-4 text-gray-500">
+          Discover the convenience of finding your perfect ride with our Ryde App
+        </Text>
+      </View>
+      <View className="px-4 mb-10">
+        <PrimaryButton text="Next" />
+      </View>
+    </SafeAreaView>
   );
 };
 
---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { Pressable, Text } from "react-native";
+
+const PrimaryButton: FC<PrimaryButtonProps> = ({ text, onPress }) => {
+  return (
+    <Pressable className="bg-blue-600 rounded-full px-4 py-5" onPress={onPress}>
+      <Text className="text-white text-center font-semibold">{text}</Text>
+    </Pressable>
+  );
+};
+
+interface PrimaryButtonProps {
+  text: string;
+  onPress?: () => void;
+}
+
+export default PrimaryButton;
