fc1099ffe603ec3828139fc8a8c125cfd66163ff
Ammar <ammar@ovasave.com>
2025-05-23 20:29:44 +0400

    build 2nd and 3rd intro screens

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,25 +1,54 @@
-import PrimaryButton from "@/src/components/PrimaryButton";
-import { FC } from "react";
-import { Image, Pressable, SafeAreaView, Text, View } from "react-native";
+import IntroTemplate from "@/src/components/intro/IntroTemplate";
+import { FC, useCallback, useState } from "react";
+import { SafeAreaView } from "react-native";
+import PagerView, { usePagerView } from "react-native-pager-view";
 
 const Welcome: FC = () => {
+  const { setPage, ref } = usePagerView();
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handleNext = useCallback(() => {
+    if (currentPage === 2) return;
+    setPage(currentPage + 1);
+  }, [currentPage, setPage]);
+
   return (
-    <SafeAreaView className="flex-1 bg-white flex-col justify-between">
-      <View className="pt-10 flex-row justify-end px-8 items-center">
-        <Pressable hitSlop={16}>
-          <Text className="font-semibold">Skip</Text>
-        </Pressable>
-      </View>
-      <View className="flex-1">
-        <Image source={require("@/assets/images/onboarding1.png")} className="w-full h-[400px]" resizeMode="contain" />
-        <Text className="text-4xl px-4 font-bold text-center mb-4">The best car in your hand with Ryde</Text>
-        <Text className="text-center px-4 text-gray-500">
-          Discover the convenience of finding your perfect ride with our Ryde App
-        </Text>
-      </View>
-      <View className="px-4 mb-10">
-        <PrimaryButton text="Next" />
-      </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <PagerView ref={ref} style={{ flex: 1 }} onPageSelected={(e) => setCurrentPage(e.nativeEvent.position)}>
+        <IntroTemplate
+          onNext={handleNext}
+          onSkip={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          title={"The best car in your hand with Ryde"}
+          subtitle={"Discover the convenience of finding your perfect ride with our Ryde App"}
+          stepNumber={1}
+          currentStep={1}
+          imageSource={require("@/assets/images/onboarding1.png")}
+        />
+        <IntroTemplate
+          onNext={handleNext}
+          onSkip={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          stepNumber={2}
+          title={"The best car in your hand with Ryde"}
+          subtitle={"Discover the convenience of finding your perfect ride with our Ryde App"}
+          currentStep={1}
+          imageSource={require("@/assets/images/onboarding2.png")}
+        />
+        <IntroTemplate
+          onNext={handleNext}
+          onSkip={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          stepNumber={3}
+          title={"The best car in your hand with Ryde"}
+          subtitle={"Discover the convenience of finding your perfect ride with our Ryde App"}
+          currentStep={1}
+          imageSource={require("@/assets/images/onboarding3.png")}
+        />
+      </PagerView>
     </SafeAreaView>
   );
 };
--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -3,7 +3,7 @@ import { Pressable, Text } from "react-native";
 
 const PrimaryButton: FC<PrimaryButtonProps> = ({ text, onPress }) => {
   return (
-    <Pressable className="bg-blue-600 rounded-full px-4 py-5" onPress={onPress}>
+    <Pressable className="bg-blue-500 rounded-full px-4 py-5" onPress={onPress}>
       <Text className="text-white text-center font-semibold">{text}</Text>
     </Pressable>
   );
