0967067d9f616cc49d2654a68a2aaae3319de95b
Ammar <ammar@ovasave.com>
2025-06-02 23:28:16 +0400

    update layout

--- src/app/(root)/(tabs)/home.tsx
+++ src/app/(root)/(tabs)/home.tsx
@@ -1,23 +1,25 @@
 import { getAuth, signOut } from "@react-native-firebase/auth";
 import { useRouter } from "expo-router";
 import { FC } from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, SafeAreaView, Text, View } from "react-native";
 
 const Home: FC = () => {
   const router = useRouter();
 
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-red-400">Home</Text>
-      <Pressable
-        onPress={async () => {
-          await signOut(getAuth());
-          router.replace("/(auth)/sign-in");
-        }}
-      >
-        <Text className="text-blue-500">Sign Out</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-red-400">Home</Text>
+        <Pressable
+          onPress={async () => {
+            await signOut(getAuth());
+            router.replace("/(auth)/sign-in");
+          }}
+        >
+          <Text className="text-blue-500">Sign Out</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
 
