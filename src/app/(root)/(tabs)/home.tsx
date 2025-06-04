3494523f397d1ab9b90a704c40c575c433bd6d7d
Ammar <ammar@ovasave.com>
2025-06-04 23:14:00 +0400

    building home search input

--- src/app/(root)/(tabs)/home.tsx
+++ src/app/(root)/(tabs)/home.tsx
@@ -1,21 +1,40 @@
+import { IconSymbol } from "@/app-example/components/ui/IconSymbol";
 import { useRouter } from "expo-router";
 import { FC } from "react";
-import { Image, SafeAreaView, Text, View } from "react-native";
-import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
+import { Image, SafeAreaView, ScrollView, Text, TextInput, View } from "react-native";
 import IconButton from "../../../../src/components/IconButton";
 
 const Home: FC = () => {
   const router = useRouter();
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView style={{ flex: 1 }}>
-        <View>
-          <Text>Welcome Ammar</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#eee" }}>
+      <ScrollView style={{ flex: 1 }} contentContainerStyle={{ paddingTop: 50 }}>
+        <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 16 }}>
+          <Text style={{ fontSize: 24, fontWeight: 500 }}>Welcome Ammar</Text>
           <IconButton onPress={() => router.push("/(root)/(tabs)/chat")}>
-            <Image source={require("@/assets/icons/out.png")} />
+            <Image source={require("@/assets/icons/out.png")} style={{ width: 30 }} resizeMode="contain" />
           </IconButton>
         </View>
+        <View
+          style={{
+            marginHorizontal: 16,
+            marginTop: 16,
+            backgroundColor: "white",
+            borderWidth: 1,
+            borderColor: "#ddd",
+            flexDirection: "row",
+            gap: 8,
+            borderRadius: 30,
+            alignItems: "center",
+            paddingHorizontal: 16,
+            marginBottom: 32,
+          }}
+        >
+          <IconSymbol name="magnifyingglass" color={"black"} />
+          <TextInput style={{ paddingVertical: 16 }} placeholder="Where do you want to go ?" />
+        </View>
+        <Text style={{ fontSize: 20, fontWeight: 500, paddingHorizontal: 16 }}>Your current location</Text>
       </ScrollView>
     </SafeAreaView>
   );
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,7 +2,22 @@ import { FC } from "react";
 import { Pressable } from "react-native";
 
 const IconButton: FC<IconButtonProps> = ({ children, onPress }) => {
-  return <Pressable onPress={onPress}>{children}</Pressable>;
+  return (
+    <Pressable
+      style={{
+        backgroundColor: "white",
+        borderRadius: "50%",
+        paddingLeft: 8,
+        paddingRight: 12,
+        paddingVertical: 4,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      onPress={onPress}
+    >
+      {children}
+    </Pressable>
+  );
 };
 
 interface IconButtonProps {
