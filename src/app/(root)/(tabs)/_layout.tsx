a6734fef768de172dcb3df85bd123fdfd8995576
Ammar <ammar@ovasave.com>
2025-06-07 21:26:34 +0400

    working on botton nav bar

--- src/app/(root)/(tabs)/_layout.tsx
+++ src/app/(root)/(tabs)/_layout.tsx
@@ -1,5 +1,82 @@
-import { Stack } from "expo-router";
+import { icons } from "@/src/constants";
+import { Tabs } from "expo-router";
+import { FC } from "react";
+import { Image, View } from "react-native";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Tabs initialRouteName="home">
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          // tabBarActiveTintColor: "white",
+          // tabBarInactiveTintColor: "white",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#333333",
+            borderRadius: 50,
+            padding: 0,
+            overflow: "hidden",
+            marginHorizontal: 20,
+            marginBottom: 20,
+            height: 68,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexDirection: "row",
+            position: "absolute",
+          },
+          tabBarIcon(props) {
+            return <TabIcon {...props} source={icons.home} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="rides"
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return <TabIcon {...props} source={icons.list} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return <TabIcon {...props} source={icons.chat} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return <TabIcon {...props} source={icons.profile} />;
+          },
+        }}
+      />
+    </Tabs>
+  );
 }
+
+const TabIcon: FC<{
+  focused: boolean;
+  color: string;
+  size: number;
+  source: any;
+}> = ({ source, focused }) => {
+  return (
+    <View
+      className={`flex flex-row items-center justify-center roudned-full ${focused ? "bg-general-300" : ""}`}
+      style={{ width: 40, height: 40, borderRadius: 20 }}
+    >
+      <View className={`rounded-full size-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}>
+        <Image source={source} tintColor={"white"} resizeMode="contain" className="w-7 h-7" />
+      </View>
+    </View>
+  );
+};
