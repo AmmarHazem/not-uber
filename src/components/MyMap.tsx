695fb2c19101e619094f92d8a253d8c27f22ff2a
Ammar <ammar@ovasave.com>
2025-06-14 16:35:46 +0400

    display drivers on map

--- src/components/MyMap.tsx
+++ src/components/MyMap.tsx
@@ -1,22 +1,72 @@
 import * as Location from "expo-location";
-import { FC, useEffect } from "react";
-import MapView from "react-native-maps";
+import { FC, useEffect, useMemo, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import MapView, { Marker, Region } from "react-native-maps";
+import { icons } from "../constants";
+import { drivers } from "../lib/drivers";
+import { generateMarkersFromData } from "../lib/map";
 
 const MyMap: FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [initialRegion, setInitialRegion] = useState<Region>();
+
   useEffect(() => {
     (async () => {
       const permissionResponse = await Location.requestForegroundPermissionsAsync();
-      console.log("Location permission response:", permissionResponse);
+      // console.log("Location permission response:", permissionResponse);
       if (permissionResponse.status !== "granted") {
-        console.error("Permission to access location was denied");
+        console.log("Permission to access location was denied");
+        setLoading(false);
         return;
       }
       const location = await Location.getCurrentPositionAsync({ accuracy: Location.LocationAccuracy.High });
-      console.log("Current location:", location);
+      // console.log("Current location:", location);
+      setInitialRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+      setLoading(false);
+      // const addresses = await Location.reverseGeocodeAsync({
+      //   latitude: location.coords.latitude,
+      //   longitude: location.coords.longitude,
+      // });
     })();
   }, []);
 
-  return <MapView style={{ width: "100%", height: 300, borderRadius: 16 }} showsUserLocation={true} />;
+  const mapDriverMarkers = useMemo(() => {
+    return generateMarkersFromData({
+      data: drivers,
+      userLatitude: initialRegion?.latitude ?? 0,
+      userLongitude: initialRegion?.longitude ?? 0,
+    });
+  }, [initialRegion?.latitude, initialRegion?.longitude]);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <MapView
+      zoomControlEnabled={true}
+      zoomEnabled={true}
+      initialRegion={initialRegion}
+      style={{ width: "100%", height: 300, borderRadius: 16 }}
+      showsUserLocation={true}
+    >
+      {mapDriverMarkers.map((driver) => {
+        return (
+          <Marker
+            key={driver.id}
+            coordinate={{ latitude: driver.latitude, longitude: driver.longitude }}
+            title={driver.title}
+            image={icons.selectedMarker}
+          />
+        );
+      })}
+    </MapView>
+  );
 };
 
 export default MyMap;
